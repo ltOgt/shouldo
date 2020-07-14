569ccbb4f7723a0efb98d777ceea844a6a6fe0e4
ltogt <omnesiaorg@gmail.com>
2020-07-14 20:15:48 +0200

    make placeholder light theme less horrendous

--- lib/page/overview/child/overview_page_content/child/task_section/child/task_widget.dart
+++ lib/page/overview/child/overview_page_content/child/task_section/child/task_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shouldo/theme/theme_notifier.dart';
+import 'package:shouldo/theme/theme_wrap.dart';
 
 class TaskWidget extends StatelessWidget {
   const TaskWidget({
@@ -23,6 +26,9 @@ class TaskWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeWrap theme =
+        Provider.of<ThemeNotifier>(context, listen: false).theme;
+
     return Container(
       height: this.height,
       decoration: BoxDecoration(
@@ -56,7 +62,7 @@ class TaskWidget extends StatelessWidget {
           ),
           Container(
             decoration: BoxDecoration(
-              color: const Color(0xFF292929), // TODO move to theme
+              color: theme.startDateColor,
               borderRadius: const BorderRadius.only(
                 topLeft: radius,
                 bottomLeft: radius,
@@ -64,13 +70,13 @@ class TaskWidget extends StatelessWidget {
             ),
             height: this.height,
             width: this.height,
-            child: Center(child: Text("$daysLeft")),
+            child: Center(child: Text("$daysSince")),
           ),
           Container(
             height: this.height,
             width: this.height,
             decoration: BoxDecoration(
-              color: const Color(0xFF2F2F2F), // TODO move to theme
+              color: theme.dueDateColor,
               borderRadius: const BorderRadius.only(
                 topRight: radius,
                 bottomRight: radius,
--- lib/theme/theme_wrap.dart
+++ lib/theme/theme_wrap.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 abstract class ThemeWrap {
   final ThemeData themeData;
+
   Color get headerColor;
+  Color get dueDateColor; // TODO [UX] add different colors for different phase
+  Color
+      get startDateColor; // TODO [UX] add different colors for different phase
+
   double get sidePadding => 22;
 
   const ThemeWrap({
@@ -13,9 +18,6 @@ abstract class ThemeWrap {
 class DarkTheme extends ThemeWrap {
   ThemeData themeData;
 
-  @override
-  Color get headerColor => const Color(0xFF1E1E1E);
-
   DarkTheme() {
     this.themeData = ThemeData.dark().copyWith(
       backgroundColor: const Color(0xFF121212),
@@ -23,15 +25,34 @@ class DarkTheme extends ThemeWrap {
       scaffoldBackgroundColor: const Color(0xFF121212),
     );
   }
+
+  @override
+  Color get headerColor => const Color(0xFF1E1E1E);
+
+  @override
+  Color get dueDateColor => const Color(0xFF2F2F2F);
+
+  @override
+  Color get startDateColor => const Color(0xFF292929);
 }
 
 class LightTheme extends ThemeWrap {
   ThemeData themeData;
 
+  LightTheme() {
+    this.themeData = ThemeData.light().copyWith(
+      backgroundColor: const Color(0xFFEFEFEF),
+      cardColor: const Color(0xFF999999),
+      scaffoldBackgroundColor: const Color(0xFFEFEFEF),
+    );
+  }
+
   @override
   Color get headerColor => const Color(0xFFCCCCCC);
 
-  LightTheme() {
-    this.themeData = ThemeData.light();
-  }
+  @override
+  Color get dueDateColor => const Color(0xFF6F6F6F);
+
+  @override
+  Color get startDateColor => const Color(0xFF696969);
 }
